ab0368aaf52f1cf2c98728d2f8fb94828b297650
pavel schudel <pavel1860@gmail.com>
2024-10-30 18:43:52 +0200

    changes to list

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Input, Listbox, ListboxItem, Skeleton, Selection } from "@nextui-org/react"
+import { Avatar, Input, Listbox, ListboxItem, Skeleton, Selection, Button } from "@nextui-org/react"
 import { Search } from "lucide-react"
 import Link from 'next/link'
 import React from "react"
@@ -19,14 +19,18 @@ export interface ListProps {
     error?: any
     items?: ListItemProps[]
     selected?: any
+    fullHeight?: boolean
+    height?: string
     onSelectionChange?: (item: any) => void
+    onAddItem?: () => void
+    addItemLabel?: string
 }
 
 
-export default function List({items, selected, onSelectionChange, loading, error}: ListProps): React.ReactElement {
+export default function List({ items, fullHeight, height, selected, onSelectionChange, addItemLabel, onAddItem, loading, error }: ListProps): React.ReactElement {
 
     const [selectedItem, setSelectedItem] = React.useState<any>(selected)
-    
+
     if (loading || !items) {
         return (<Skeleton className="flex rounded-full w-12 h-12">
             <div className="h-24 rounded-lg bg-secondary"></div>
@@ -38,7 +42,7 @@ export default function List({items, selected, onSelectionChange, loading, error
         return <div>Error: {error.message}</div>
     }
 
-    
+
 
     return (
         <div>
@@ -48,19 +52,34 @@ export default function List({items, selected, onSelectionChange, loading, error
                 disallowEmptySelection
                 selectionMode="single"
                 selectedKeys={[selected]}
-                topContent={<Input
-                    label="Search"
-                    isClearable
-                    radius="lg"
+                topContent={
+                    <div className="">
+                        {onAddItem && 
+                            <Button 
+                                variant="ghost" 
+                                color="primary" 
+                                size="sm" 
+                                onClick={()=>{
+                                    onAddItem()
+                                }}
+                            > {addItemLabel || "Add"}</Button>}
+                        <Input
+                            label="Search"
+                            isClearable
+                            radius="lg"
 
-                    startContent={
-                        <Search color="gray" />
-                    }
+                            startContent={
+                                <Search color="gray" />
+                            }
 
-                />}
+                        />
+                        
+                    </div>
+                }
                 classNames={{
                     base: "max-w-xs",
-                    list: "max-h-[300px] overflow-scroll",
+                    // list: "max-h-[300px] overflow-scroll",
+                    list: fullHeight ? "h-full overflow-scroll" : height ? `max-h-[${height}px] overflow-scroll` : "max-h-[300px] overflow-scroll",
                 }}
 
                 onSelectionChange={(keys: Selection) => {
@@ -71,22 +90,22 @@ export default function List({items, selected, onSelectionChange, loading, error
                 }}
                 items={items}
                 variant="flat"
-                // selectionMode="single"
+            // selectionMode="single"
             >
                 {(item) => (
-                    <ListboxItem 
+                    <ListboxItem
                         key={item.key}
                         href={item.href}
                         title={item.title}
-                        description={item.description} 
-                        
+                        description={item.description}
+
                         // className={selectedItem === item.key ? "bg-slate-300" : ''} 
                         // classNames={selectedItem === item.key ?{
                         //     base: "bg-slate-300"
                         // }: {}}
-                        showDivider 
-                        // shouldHighlightOnFocus
-                        >
+                        showDivider
+                    // shouldHighlightOnFocus
+                    >
                         <span className="text-small">{item.value}</span>
                     </ListboxItem>
                 )}
