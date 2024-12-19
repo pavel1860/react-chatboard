d6e5059612dd1742f216372f648bd5d306ee74b6
pavel schudel <pavel1860@gmail.com>
2024-12-19 15:58:07 +0200

    added dynamic form

---
+++ src/components/form/form.tsx
@@ -0,0 +1,376 @@
+// src/components/DynamicForm.tsx
+import React, { useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { ZodSchema, ZodTypeAny, z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input, Select, SelectItem, Skeleton, Form, Button } from '@nextui-org/react';
+
+
+type FieldConfig = {
+    [key: string]: {
+        hidden?: boolean;
+        omit?: boolean;
+        disabled?: boolean;
+        icon?: React.ReactElement;
+    }
+}
+
+type DynamicFormProps<T extends ZodTypeAny> = {
+    schema: T;
+    defaultValues?: Partial<z.infer<T>>; // Optional default values
+    onSubmit: SubmitHandler<z.infer<T>>;
+    loading?: boolean;
+    isReadOnly?: boolean;
+    // variant?: "solid" | "bordered" | "light" | "flat" | "faded" | "shadow" | undefined
+    variant?: "flat" | "bordered" | "faded" | "underlined"
+    controls?: "top" | "bottom"
+    iconPlacement?: "left" | "inputStart" | "inputEnd"
+    fieldConfig?: FieldConfig,
+    radius?: "none" | "sm" | "md" | "lg" | "full" | undefined
+    size: "sm" | "md" | "lg"
+    classNames?: {
+        label?: string,
+        input?: string
+    }
+    labelPlacement?: "inside" | "outside" | "outside-left"
+};
+
+
+// interface EditableFieldProps {
+//     fieldName: string
+//     fieldType: string
+//     label: string
+//     register: any
+//     errors: any
+// }
+
+// export function EditableField<T>({fieldName, register, label}: EditableFieldProps){
+//     <div key={fieldName}>
+//         <label htmlFor={fieldName}>{label}</label>
+//         <input id={fieldName} {...register(fieldName as keyof z.infer<T>)} />
+//         {errors[fieldName] && <p style={{ color: 'red' }}>{errors[fieldName]?.message as string}</p>}
+//     </div>
+// }
+
+const FieldWrapper = ({ children, icon, error }: any) => {
+
+    return (
+        <div>
+            <div className="flex items-center gap-2">
+                {icon}
+                {children}
+            </div>
+            {error && <p style={{ color: 'red' }}>{error?.message as string}</p>}
+        </div>
+    )
+}
+
+
+
+const getOmitFields = (schema: any, fieldConfig?: FieldConfig) => {
+    const shape = schema.shape;
+    let omitFields: any = {}
+    if (fieldConfig){        
+        for (const fieldName in fieldConfig) {
+            if (fieldConfig[fieldName].omit) {
+                omitFields[fieldName] = true
+            }
+        }
+        return schema.omit(omitFields)
+    } else {
+        return schema
+    }
+}
+
+
+const DynamicForm = <T extends ZodTypeAny>(
+    {
+        schema,
+        loading,
+        onSubmit,
+        fieldConfig,
+        variant,
+        size,
+        isReadOnly: isReadOnlyProp,
+        controls,
+        radius,
+        defaultValues = {}, // Default to empty object if not provided
+        labelPlacement = "outside-left",
+        iconPlacement = "left",
+        classNames,
+    }: DynamicFormProps<T>) => {
+
+
+    const [isReadOnly, setIsReadOnly] = useState(isReadOnlyProp !== undefined ? isReadOnlyProp : true)
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm<z.infer<T>>({
+        // resolver: zodResolver(schema.omit({
+        //     created_at: true,
+        //     updated_at: true,
+        //     id: true,
+        //     phone_number: true,
+        //     manager_phone_number: true,
+        // })),
+        resolver: zodResolver(getOmitFields(schema, fieldConfig)),
+        defaultValues: defaultValues,
+    });
+
+
+    const onSubmitHandler: SubmitHandler<z.infer<typeof schema>> = (data) => {
+        setIsReadOnly(true)
+        onSubmit(data)
+        // console.log(data)
+    }
+
+
+
+    if (loading) {
+        const shape = schema.shape;
+        return (
+            <div className="w-full flex flex-col gap-2 px-4 items-center">
+                {Object.keys(shape).map((fieldName) => (
+                    <Skeleton className="h-4 m-2 w-2/5 rounded-lg" />
+                ))}
+            </div>
+        )
+    }
+
+
+    // Helper function to render form fields based on schema
+    const renderFields = () => {
+        const shape = schema.shape;
+        return Object.keys(shape).map((fieldName) => {
+            const field = shape[fieldName];
+            const fieldType = getFieldType(field);
+            const label = toTitleCase(fieldName);
+
+            let inputStartIcon = undefined
+            let inputEndIcon = undefined
+            let leftIcon = undefined
+
+            const currFieldConfig = fieldConfig?.[fieldName]
+            if (currFieldConfig) {
+                if (currFieldConfig.hidden || currFieldConfig.omit) return null;
+                if (iconPlacement == "left") {
+                    leftIcon = currFieldConfig.icon
+                } else if (iconPlacement == "inputStart") {
+                    inputStartIcon = currFieldConfig.icon
+                } else if (iconPlacement == "inputEnd") {
+                    inputEndIcon = currFieldConfig.icon
+                }
+            }
+
+            // Skip hidden fields
+            // if (fieldConfig?.[fieldName]?.hidden) return null;
+
+
+            switch (fieldType) {
+                case 'string':
+                    return (
+                        <FieldWrapper icon={leftIcon} error={errors[fieldName]}>
+                            <Controller
+                                name={fieldName as keyof z.infer<T>}
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        isReadOnly={isReadOnly}
+                                        id={fieldName}
+                                        variant={variant}
+                                        radius={radius}
+                                        size={size}
+                                        startContent={inputStartIcon}
+                                        endContent={inputEndIcon}
+                                        {...field}
+                                        label={label}
+                                        placeholder={label}
+                                        labelPlacement={labelPlacement}
+                                        classNames={{
+                                            label: classNames?.label,
+                                            mainWrapper: `${classNames?.input}`,
+                                            // inputWrapper: `${isReadOnly ? "bg-white" : ""}`
+                                        }}
+                                    />
+                                )}
+
+                            />
+                        </FieldWrapper>
+                    );
+                case 'number':
+                    return (
+                        <FieldWrapper icon={leftIcon} error={errors[fieldName]}>
+                            <Controller
+                                name={fieldName as keyof z.infer<T>}
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        id={fieldName}
+                                        isReadOnly={isReadOnly}
+                                        variant={variant}
+                                        startContent={inputStartIcon}
+                                        endContent={inputEndIcon}
+                                        type="number"
+                                        size={size}
+                                        radius={radius}
+                                        {...field}
+                                        label={label}
+                                        placeholder={label}
+                                        labelPlacement={labelPlacement}
+                                        classNames={{
+                                            label: classNames?.label,
+                                            mainWrapper: `${classNames?.input}`,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </FieldWrapper>)
+
+                case 'enum':
+                    // const enumValues = (field as z.ZodEnum<[string, ...string[]]>).options;
+                    const enumValues = (field as z.ZodEnum<[string, ...string[]]>).enum;
+                    const selectValues = Object.keys(enumValues).map((option) => ({ label: toTitleCase(option), value: enumValues[option] }));
+                    return (
+                        <FieldWrapper icon={leftIcon} error={errors[fieldName]}>
+                            <Controller
+                                name={fieldName as keyof z.infer<T>}
+                                control={control}
+                                render={({ field }) => (
+                                    isReadOnly ?
+                                        <Input
+                                            id={fieldName}
+                                            isReadOnly={isReadOnly}
+                                            variant={variant}
+                                            startContent={inputStartIcon}
+                                            endContent={inputEndIcon}
+                                            size={size}
+                                            radius={radius}
+                                            // {...field}
+                                            value={field.value}
+                                            label={label}
+                                            placeholder={label}
+                                            labelPlacement={labelPlacement}
+                                            classNames={{
+                                                label: classNames?.label,
+                                                mainWrapper: `${classNames?.input}`,
+                                            }}
+                                        /> :
+                                        <Select
+                                            id={fieldName}
+                                            disallowEmptySelection
+                                            isDisabled={isReadOnly}
+                                            variant={variant}
+                                            size={size}
+                                            radius={radius}
+                                            startContent={inputStartIcon}
+                                            endContent={inputEndIcon}
+                                            selectedKeys={[field.value]}
+                                            {...field}
+                                            items={selectValues}
+                                            label={label}
+                                            placeholder={label}
+                                            labelPlacement={labelPlacement}
+                                            classNames={{
+                                                base: "items-center",
+                                                mainWrapper: `${classNames?.input}`,
+                                                label: `ps-2 pe-2 ${classNames?.label}`,
+                                            }}
+                                        // items={enumValues.map((option) => ({ label: toTitleCase(option), value: option }))}
+                                        >
+                                            {
+                                                selectValues.map((option) => (<SelectItem key={option.value}>{option.label}</SelectItem>))
+                                            }
+                                            {/* {Object.keys(enumValues).map((option) => (
+                                            <SelectItem key={option}>{enumValues[option]}</SelectItem>
+                                        ))} */}
+                                        </Select>
+
+                                )}
+                            />
+                        </FieldWrapper>)
+                case 'boolean':
+                    return (
+                        <div key={fieldName}>
+                            <label htmlFor={fieldName}>
+                                <input type="checkbox" id={fieldName} {...register(fieldName as keyof z.infer<T>)} />
+                                {label}
+                            </label>
+                            {errors[fieldName] && <p style={{ color: 'red' }}>{errors[fieldName]?.message as string}</p>}
+                        </div>
+                    );
+                default:
+                    return null;
+            }
+        });
+    };
+
+    return <div>
+        {controls && <div className="flex justify-end w-full">
+            {
+                isReadOnly ? <Button
+                    size="sm"
+                    variant='light'
+                    onPress={() => { setIsReadOnly(false) }}
+                >
+                    edit
+                </Button> : null
+                    
+            }
+        </div>}
+        {/* {errors && <div className="text-red-500">{JSON.stringify(errors)}</div>} */}
+        <Form
+            className="w-full max-w-md py-2 px-4"
+            validationBehavior="native"
+            onSubmit={handleSubmit(onSubmitHandler)} // Correct usage
+        >
+            {controls && <div className="flex justify-end w-full">
+                {
+                    isReadOnly ? null:
+                        <>
+                            <Button
+                                type="submit"
+                                size="sm"
+                                variant='light'
+                            >
+                                save
+                            </Button>
+                            <Button
+                                size="sm"
+                                variant='light'
+                                onPress={() => { setIsReadOnly(true) }}
+                            >
+                                cancel
+                            </Button>
+                        </>
+
+                }
+            </div>}
+            {renderFields()}
+        </Form>
+    </div>
+};
+
+// Utility function to determine field type
+const getFieldType = (field: ZodTypeAny): string => {
+    if (field instanceof z.ZodString) return 'string';
+    if (field instanceof z.ZodNumber) return 'number';
+    if (field instanceof z.ZodBoolean) return 'boolean';
+    if (field instanceof z.ZodEnum) return 'enum';
+    if (field instanceof z.ZodNativeEnum) return 'enum';
+    // Add more types as needed
+    return 'string';
+};
+
+// Utility function to convert field names to Title Case
+const toTitleCase = (str: string) => {
+    return str
+        .replace(/([A-Z])/g, ' $1') // Add space before capital letters
+        .replace(/^./, (s) => s.toUpperCase())
+        .replace("_", " ") // Capitalize first letter
+        .trim();
+};
+
+export default DynamicForm;
--- src/services/model-service.tsx
+++ src/services/model-service.tsx
@@ -12,7 +12,7 @@ interface FetcherOptions<T> {
     queryParams?: Record<string, any>;
 }
 
-export async function fetcher<T>({ schema, endpoint, queryParams }: FetcherOptions<T>): Promise<T> {
+export async function fetcher<T>({ schema, endpoint, queryParams }: FetcherOptions<T>): Promise<T | null> {
     let url = `${endpoint}`;
 
     if (queryParams) {
@@ -31,6 +31,9 @@ export async function fetcher<T>({ schema, endpoint, queryParams }: FetcherOptio
 
     const data = await res.json();
     // return schema.parse(data); // Validate data using Zod schema
+    if (data == null || data == undefined) {
+        return null
+    }
     const result = schema.safeParse(data);
     if (result.success) {
         return result.data;
@@ -71,19 +74,21 @@ export async function sendRequest<T, P>({ schema, endpoint, data }: MutationOpti
 
 interface UseMutationOptions<T, P> {
     schema: ZodSchema<P>;
-    model: string;
-    id?: string;
+    // model: string;
+    endpoint: string;
+    // id?: string;
     callbacks?: {
         onSuccess?: (data: P) => void;
         onError?: (error: any) => void;
     };
 }
 
-export function useMutationHook<T, P>({ schema, model, id, callbacks }: UseMutationOptions<T, P>): SWRMutationResponse<P, Error> {
-    const endpoint = id ? `${model}/update/${id}` : `${model}/create`;
+export function useMutationHook<T, P>({ schema, endpoint, callbacks }: UseMutationOptions<T, P>): SWRMutationResponse<P, Error> {
+    // const endpoint = id ? `${model}/update/${id}` : `${model}/create`;
 
     const { trigger, data, error, isMutating, reset } = useSWRMutation<P>(
-        `/api/model/${endpoint}`,
+        // `/api/model/${endpoint}`,
+        endpoint,
         async (url: string, { arg }: { arg: T }) => {
             const response = await sendRequest<T, P>({ schema, endpoint, data: arg });
             return response;
@@ -110,7 +115,7 @@ export default function createModelService<T>(model: string, schema: ZodSchema<T
 
 
     function useGetModel(id: string) {
-        return useSWR<T>(`${baseUrl}/${model}/${id}`, (url: string) => fetcher({ schema, endpoint: url }));
+        return useSWR<T | null>(`${baseUrl}/${model}/${id}`, (url: string) => fetcher({ schema, endpoint: url }));
     }
 
     function useGetModelList(partitions?: any, limit: number = 10, offset: number = 0) {
@@ -119,15 +124,15 @@ export default function createModelService<T>(model: string, schema: ZodSchema<T
     }
 
     function useLastModel(partitions: any) {
-        return useSWR<T>([`${baseUrl}/${model}/last`, partitions], ([url, partitions]) => fetcher({ schema, endpoint: url, queryParams: partitions }));
+        return useSWR<T | null>([`${baseUrl}/${model}/last`, partitions], ([url, partitions]) => fetcher({ schema, endpoint: url, queryParams: partitions }));
     }
 
     function useCreateModel() {
-        return useMutationHook<T, T>({ schema, model });
+        return useMutationHook<T, T>({ schema, endpoint: `${baseUrl}/${model}/create` });
     }
 
     function useUpdateModel(id: string) {
-        return useMutationHook<T, T>({ schema, model, id });
+        return useMutationHook<T, T>({ schema, endpoint: `${baseUrl}/${model}/update/${id}` });
     }
 
     return {
@@ -142,60 +147,60 @@ export default function createModelService<T>(model: string, schema: ZodSchema<T
 
 
 
-export function useGetModel<T>(model: string, id: string) {
-    const url = `/${model}` + "/" + id
-    const { data, error, isLoading, mutate } = useSWR(url, (url: string) => fetcher(url))
+// export function useGetModel<T>(model: string, id: string) {
+//     const url = `/${model}` + "/" + id
+//     const { data, error, isLoading, mutate } = useSWR(url, (url: string) => fetcher(url))
 
-    return {
-        data,
-        error,
-        isLoading,
-        mutate
-    }
-}
-
-
-export function useGetModelList(model: string, partitions?: any, limit: number = 10, offset: number = 0,) {
-
-    const url = `/${model}/list`
-    const { data, error, isLoading, mutate } = useSWR(partitions ? [url, partitions, limit, offset] : null, ([url, partitions, limit, offset]) => fetcher(url, { ...partitions, limit, offset }))
-
-    return {
-        data: data || [],
-        error,
-        isLoading,
-        mutate
-    }
-}
+//     return {
+//         data,
+//         error,
+//         isLoading,
+//         mutate
+//     }
+// }
 
 
+// export function useGetModelList(model: string, partitions?: any, limit: number = 10, offset: number = 0,) {
 
-export function useLastModel(model: string, partitions: any) {
-    const url = `/${model}/last`
-    const { data, error, isLoading, mutate } = useSWR(partitions ? [url, partitions] : null, ([url, partitions]) => fetcher(url, partitions))
-    // const { data, error, isLoading, mutate } = useSWR([url], ([url]) => fetcher(url, {}))
+//     const url = `/${model}/list`
+//     const { data, error, isLoading, mutate } = useSWR(partitions ? [url, partitions, limit, offset] : null, ([url, partitions, limit, offset]) => fetcher(url, { ...partitions, limit, offset }))
 
-    return {
-        data,
-        error,
-        isLoading,
-        mutate
-    }
-}
-
-
-export function useUpdateModel<T>(model: string, id: string) {
-    const url = `/${model}/update/` + id
-    const {
-        isMutating,
-        trigger
-    } = useMutation(url)
+//     return {
+//         data: data || [],
+//         error,
+//         isLoading,
+//         mutate
+//     }
+// }
 
 
-    return {
-        isMutating,
-        trigger
-    }
-}
+
+// export function useLastModel(model: string, partitions: any) {
+//     const url = `/${model}/last`
+//     const { data, error, isLoading, mutate } = useSWR(partitions ? [url, partitions] : null, ([url, partitions]) => fetcher(url, partitions))
+//     // const { data, error, isLoading, mutate } = useSWR([url], ([url]) => fetcher(url, {}))
+
+//     return {
+//         data,
+//         error,
+//         isLoading,
+//         mutate
+//     }
+// }
+
+
+// export function useUpdateModel<T>(model: string, id: string) {
+//     const url = `/${model}/update/` + id
+//     const {
+//         isMutating,
+//         trigger
+//     } = useMutation(url)
+
+
+//     return {
+//         isMutating,
+//         trigger
+//     }
+// }
 
 
